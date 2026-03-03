abb79df703bac6431fd914138c825ddc0224f658
zeedkaiser <168396606+zeedkaiser@users.noreply.github.com>
2026-03-04 00:26:33 +0700

    Ganti Verify 2

--- oauthServer.js
+++ oauthServer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const session = require("express-session");
 const { upsertMember } = require("./database");
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 
 
 const app = express();
